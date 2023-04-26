6e91317c26668b9de1d1de662a0b87090322148e
Lord Ghapa <felipef32@hotmail.com>
2023-04-26 14:18:05 -0300

    feat:render img when finish all task

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
-const defaultList = {
-  to_do_list: [
-    {
-      title: '🛒 Buy groceries',
-      description: 'Milk, bread, eggs, cheese, fruits and vegetables.',
-      completed: false
-    },
-    {
-      title: '📝 Finish project',
-      description: 'Complete the final report and submit it by Friday.',
-      completed: true
-    },
-    {
-      title: '🧼 Clean the house',
-      description: 'Vacuum, dust, and clean the bathrooms.',
-      completed: false
-    },
-    {
-      title: '💰 Pay bills',
-      description: 'Electricity, water, and internet bills.',
-      completed: false
-    },
-    {
-      title: '📞 Call mom',
-      description: "Check in and see how she's doing.",
-      completed: true
-    },
-    {
-      title: '📅 Schedule appointment',
-      description: 'Make an appointment with the dentist.',
-      completed: true
-    },
-    {
-      title: '🏃‍♂️ Go for a run',
-      description: 'Jog for 30 minutes.',
-      completed: true
-    },
-    {
-      title: '📖 Read a book',
-      description: "Finish reading 'The Great Gatsby'.",
-      completed: true
-    },
-    {
-      title: '✍️ Write a blog post',
-      description: 'Come up with a topic and write a 500-word blog post.',
-      completed: true
-    },
-    {
-      title: '🧹 Organize closet',
-      description:
-        'Sort clothes by category and donate items no longer needed.',
-      completed: true
-    }
-  ]
-}
 let list = JSON.parse(localStorage.getItem('list'))
 const toDo = document.querySelector('#to-do')
 const done = document.querySelector('#done')
 const form = document.querySelector('#form')
+
+const defaultList = {
+  to_do_list: [
+    {
+      title: '🛒 Comprar mantimentos',
+      description: 'Leite, pão, ovos, queijo, frutas e legumes.',
+      completed: false
+    },
+    {
+      title: '📝 Terminar projeto',
+      description: 'Concluir o relatório final e enviá-lo até sexta-feira.',
+      completed: true
+    },
+    {
+      title: '🧼 Limpar a casa',
+      description: 'Aspirar, tirar o pó e limpar os banheiros.',
+      completed: false
+    },
+    {
+      title: '💰 Pagar contas',
+      description: 'Contas de eletricidade, água e internet.',
+      completed: false
+    },
+    {
+      title: '📞 Ligar para a mãe',
+      description: 'Verificar como ela está.',
+      completed: true
+    },
+    {
+      title: '📅 Agendar consulta',
+      description: 'Marcar uma consulta com o dentista.',
+      completed: true
+    },
+    {
+      title: '🏃‍♂️ Fazer uma corrida',
+      description: 'Correr por 30 minutos.',
+      completed: true
+    },
+    {
+      title: '📖 Ler um livro',
+      description: "Terminar de ler 'O Grande Gatsby'.",
+      completed: true
+    },
+    {
+      title: '✍️ Escrever um post de blog',
+      description:
+        'Criar um tópico e escrever um post de blog com 500 palavras.',
+      completed: true
+    },
+    {
+      title: '🧹 Organizar o armário',
+      description:
+        'Organizar as roupas por categoria e doar itens não mais necessários.',
+      completed: true
+    }
+  ]
+}
+
 if (!list) {
   localStorage.setItem('list', JSON.stringify(defaultList))
   list = JSON.parse(localStorage.getItem('list'))
@@ -65,12 +68,18 @@ if (!list) {
 function getListData() {
   toDo.innerHTML = ''
   done.innerHTML = ''
+
   let lastItem
 
   list.to_do_list.forEach((itm, index) => {
     renderToDoItem(itm, index)
+
     lastItem = index
   })
+
+  if (toDo.childElementCount === 0) {
+    renderCompletedImg()
+  }
 
   lastItem++
   createNewItemFields(lastItem)
@@ -81,14 +90,16 @@ function renderToDoItem(itm, index) {
   title.name = `n${index}-title`
   title.value = itm.title
   title.maxLength = 30
+  title.addEventListener('change', handleItemsSubmit)
+
   const desc = document.createElement('textarea')
   desc.name = `n${index}-desc`
   desc.value = itm.description
-
   desc.style.height = '1.5rem'
-  desc.style.height = this.scrollHeight + 'xp'
+  desc.style.height = this.scrollHeight + 'px'
   desc.addEventListener('input', autoResize, false)
   desc.addEventListener('click', autoResize, false)
+  desc.addEventListener('change', handleItemsSubmit)
 
   const completed = document.createElement('input')
   completed.type = 'hidden'
@@ -105,8 +116,9 @@ function renderToDoItem(itm, index) {
 
   const checkBtn = document.createElement('span')
   checkBtn.classList.add('material-symbols-outlined')
-  checkBtn.title = 'segure para apaga este item'
+  checkBtn.title = 'Segure para apaga este item'
   checkBtn.addEventListener('long-press', removeItem)
+  checkBtn.addEventListener('click', checkItem)
   checkBtn.setAttribute('data-long-press-delay', '100')
   checkBtn.id = index
 
@@ -114,13 +126,14 @@ function renderToDoItem(itm, index) {
   wrapper.appendChild(checkBtn)
 
   if (itm.completed) {
-    checkBtn.innerHTML = 'check_box'
+    checkBtn.innerText = 'check_box'
     done.appendChild(wrapper)
   } else {
-    checkBtn.innerHTML = 'check_box_outline_blank'
+    checkBtn.innerText = 'check_box_outline_blank'
     toDo.appendChild(wrapper)
   }
 }
+
 function autoResize() {
   this.style.height = '1.5rem'
   this.style.height = this.scrollHeight + 'px'
@@ -130,17 +143,17 @@ function createNewItemFields(index) {
   const newItmWrapper = document.createElement('div')
   const newItmData = document.createElement('div')
   const newTitle = document.createElement('textarea')
+
   newTitle.name = `n${index}-title`
   newTitle.maxLength = 30
-  newTitle.placeholder = '🌟 Crie Uma Nova Tarefa...'
-
+  newTitle.placeholder = '✨ Crie um nova Tarefa...'
   newTitle.addEventListener('change', handleItemsSubmit)
 
   const newDesc = document.createElement('textarea')
   newDesc.name = `n${index}-desc`
   newDesc.style.height = '1.5rem'
   newDesc.style.height = this.scrollHeight + 'px'
-  newDesc.placeholder = 'Note description'
+  newDesc.placeholder = 'Descrição'
   newDesc.addEventListener('input', autoResize, false)
   newDesc.addEventListener('change', autoResize, false)
   newDesc.addEventListener('change', handleItemsSubmit)
@@ -161,7 +174,6 @@ function createNewItemFields(index) {
 function handleItemsSubmit(e) {
   const formData = new FormData(form)
 
-  // Creating an empty to-do list
   const todoObject = {
     to_do_list: []
   }
@@ -191,9 +203,11 @@ function handleItemsSubmit(e) {
     getListData()
   }
 }
+
 function saveItemsStorage() {
   localStorage.setItem('list', JSON.stringify(list))
 }
+
 function removeItem(e) {
   let idToRemove = e.target.id
   const filteredList = list.to_do_list.filter((itm, index) => {
@@ -207,9 +221,40 @@ function removeItem(e) {
     translateX: [0, 50],
     opacity: [1, 0],
     easing: 'easeInExpo',
-    completed: function (anim) {
+    complete: function (anim) {
       getListData()
       saveItemsStorage()
     }
   })
+}
+
+function checkItem(e) {
+  let isCompleted = list.to_do_list[e.target.id].completed
+  list.to_do_list[e.target.id].completed = !isCompleted
+
+  anime({
+    targets: e.target.parentElement,
+    duration: 500,
+    translateX: [0, 50],
+    opacity: [1, 0],
+    easing: 'easeInExpo',
+    complete: function (anim) {
+      getListData()
+      saveItemsStorage()
+    }
+  })
+}
+
+function renderCompletedImg() {
+  const upToDateImg = document.createElement('img')
+  const upToDateText = document.createElement('h3')
+
+  upToDateImg.src = './img/upToDate.svg'
+  upToDateImg.classList.add('up-to-date-img')
+
+  upToDateText.innerText = 'Todas as Tarefas Completas'
+  upToDateText.classList.add('up-to-date-txt')
+
+  toDo.appendChild(upToDateImg)
+  toDo.appendChild(upToDateText)
 }
